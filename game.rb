1b2deac6c16196991d5dee8b7b9e56a2478ef8f0
rodgoncarmen2005 <rodgoncarmen@correo.ugr.es>
2025-11-05 10:17:53 +0100

    player y game

---
+++ game.rb
@@ -0,0 +1,183 @@
+#encoding:utf-8
+
+require_relative 'player'
+require_relative 'monster'
+require_relative 'labyrinth'
+require_relative 'dice'
+require_relative 'game_state'
+require_relative 'directions'
+require_relative 'game_character'
+require_relative 'orientation'
+
+module Irrgarten 
+	class Game
+	  @@MAX_ROUNDS = 10
+	  @@ROWS = 10
+	  @@COLUMNS = 10
+	  @@NUM_MONSTERS = 2
+	  @@NUM_BLOCKS = 2
+
+	  attr_reader :current_player_index, :log, :current_player, :players, :monsters, :labyrinth
+
+	  def initialize(nplayers)
+		@players = []
+		@monsters = []
+
+		for (int i = 0; i < nplayers; ++i)
+		  p = Player.new(i, Dice.random_intelligence, Dice.random_strength)
+		  @players << p
+		end
+
+		@current_player_index = Dice.who_starts(nplayers)
+		@current_player = @players[@current_player_index]
+
+		@log = "The game begins\n"
+
+		@labyrinth = Labyrinth.new(ROWS, COLUMNS, Dice.random_pos(ROWS), Dice.random_pos(COLUMNS))
+		configure_labyrinth
+		@labyrinth.spread_players(@players)
+		@log = "Empieza el juego"
+	  end
+
+	  def finished
+		@labyrinth.have_a_winner
+	  end
+
+	  def next_step(preferred_direction)
+		@log = ""
+
+		dead = @current_player.dead?
+
+		if !dead
+		  direction = actual_direction(preferred_direction)
+		  log_player_no_orders if direction != preferred_direction
+
+		  monster = @labyrinth.put_player(direction, @current_player)
+
+		  if monster.nil?
+		    log_no_monster
+		  else
+		    winner = combat(monster)
+		    manage_reward(winner)
+		  end
+		else
+		  manage_resurrection
+		end
+
+		end_game = finished
+		next_player unless end_game
+		end_game
+	  end
+
+	  def game_state
+		players_str = @players.map { |p| "+ #{p}\n" }.join
+		monsters_str = @monsters.map { |m| "+ #{m}\n" }.join
+
+		GameState.new(@labyrinth.to_s, players_str, monsters_str, @current_player_index, finished, @log)
+	  end
+
+	  private
+
+	  def configure_labyrinth
+		NUM_MONSTERS.times do |i|
+		  monster = Monster.new("Monster#{i}", Dice.random_intelligence, Dice.random_strength)
+		  @monsters << monster
+		  @labyrinth.add_monster(Dice.random_pos(ROWS), Dice.random_pos(COLUMNS), monster)
+		end
+
+		# Bloques prefijados
+		@labyrinth.add_block(Orientation::HORIZONTAL, 5, 4, 2)
+		@labyrinth.add_block(Orientation::VERTICAL, 9, 8, 1)
+	  end
+
+	  def next_player
+		if @current_player_index == @players.size - 1
+		  @current_player_index = 0
+		else
+		  @current_player_index += 1
+		end
+		@current_player = @players[@current_player_index]
+	  end
+
+	  def actual_direction(preferred_direction)
+		current_row = @current_player.row
+		current_col = @current_player.col
+		valid_moves = @labyrinth.valid_moves(current_row, current_col)
+		@current_player.move(preferred_direction, valid_moves)
+	  end
+
+	  def combat(monster)
+		rounds = 0
+		winner = GameCharacter::PLAYER
+
+		player_attack = @current_player.attack
+		lose = monster.defend(player_attack)
+
+		while !lose && rounds < MAX_ROUNDS
+		  winner = GameCharacter::MONSTER
+		  rounds += 1
+
+		  monster_attack = monster.attack
+		  lose = @current_player.defend(monster_attack)
+
+		  unless lose
+		    player_attack = @current_player.attack
+		    winner = GameCharacter::PLAYER
+		    lose = monster.defend(player_attack)
+		  end
+		end
+
+		log_rounds(rounds, MAX_ROUNDS)
+		winner
+	  end
+
+	  def manage_reward(winner)
+		if winner == GameCharacter::PLAYER
+		  @current_player.received_reward
+		  log_player_won
+		else
+		  log_monster_won
+		end
+	  end
+
+	  def manage_resurrection
+		resurrect = Dice.resurrect_player
+		if resurrect
+		  @current_player.resurrect
+		  log_resurrected
+		else
+		  log_player_skip_turn
+		end
+	  end
+
+	  # Métodos de logging
+
+	  def log_player_won
+		@log << "Winner: player #{@current_player_index}\n"
+	  end
+
+	  def log_monster_won
+		@log << "Winner: monster\n"
+	  end
+
+	  def log_resurrected
+		@log << "Resurrected: player #{@current_player_index}\n"
+	  end
+
+	  def log_player_skip_turn
+		@log << "Player #{@current_player_index} skipped turn due to death\n"
+	  end
+
+	  def log_player_no_orders
+		@log << "The instruction for player #{@current_player_index} could not be followed.\n"
+	  end
+
+	  def log_no_monster
+		@log << "Player #{@current_player_index} moved to an empty square or it was not possible to move.\n"
+	  end
+
+	  def log_rounds(rounds, max)
+		@log << "Round #{rounds} out of #{max}\n"
+	  end
+	end
+
---
+++ player.rb
@@ -0,0 +1,174 @@
+#encoding:utf-8
+
+require_relative 'dice'
+require_relative 'weapon'
+require_relative 'shield'
+
+module Irrgarten
+  class Player
+    
+    @@MAX_WEAPONS = 2
+    @@MAX_SHIELDS = 3
+    @@INITIAL_HEALTH = 10
+    @@HITS2LOSE = 3
+  #FALTAN
+
+
+  def initialize(number, intelligence, strength)
+    @number = number.to_s
+    @name = "Player #{@number}"
+    @intelligence = intelligence
+    @strength = strength
+    @health = @@INITIAL_HEALTH
+
+    @weapons = Array.new
+    @shields = Array.new
+    
+    @consecutive_hits = 0
+
+    @row = -1
+    @column = -1
+  end
+
+
+
+  def resurrect
+    if Dice.resurrect_player
+      @weapons.clear
+      @shields.clear
+      @health = @@INITIAL_HEALTH
+      reset_hits
+    end
+  end
+
+  def get_row
+    @row
+  end
+
+  def get_column
+    @column
+  end
+  
+  def get_number
+    @number
+  end
+
+  def set_pos(row, column)
+    @row = row
+    @column = column
+  end
+
+  def dead
+    @health > 0
+  end
+
+  def move (direction, valid_moves)
+    size = valid_moves.size
+    contained = valid_moves.include?(direction)
+
+    if size > 0 && contained
+      first_element = valid_moves[0]
+      return first_element
+    else
+      return direction
+    end
+  end
+
+  def attack
+    @strength + sum_weapons
+  end
+
+  def defend(received_attack)
+    manage_hits(received_attack)
+  end
+
+  def received_reward
+    w_reward = Dice.weapon_reward
+    s_reward = Dice.shield_reward
+
+    w_reward.times do
+      w_new = Weapon.new
+      received_weapon(w_new)
+    end
+
+    s_reward.times do
+      s_new = Shield.new
+      received_shield(s_new)
+    end
+
+    extra_health = Dice.health_reward
+    @health += extra_health
+  end
+
+  def to_s
+    "Player #{@number} - Health: #{@health}, Weapons: #{sum_weapons}, Shields: #{sum_shields}, Pos: (#{@row}, #{@column})"
+  end
+
+
+  private
+
+  def received_weapon(w)
+
+  end
+
+  def received_shield(s)
+
+  end
+
+  def new_weapon
+    w = Weapon.new(Dice.weapon_power, Dice.uses_left)
+  end
+
+  def new_shield
+    s = Shield.new(Dice.shield_power, Dice.uses_left)
+  end
+
+  def sum_weapons
+    sum = 0
+    @weapons.each do |w|
+      sum += w.attack
+    end
+    sum
+  end
+
+  def sum_shields
+    sum = 0
+    @shields.each do |s|
+      sum += s.protect
+    end
+    sum
+  end
+
+  def defensive_energy
+    @intelligence + sum_shields
+  end
+
+  def manage_hits(received_attack)
+    defense = self.defensive_energy
+    lose = false;
+
+    if defense < received_attack
+      self.got_wounded
+      self.inc_consecutive_hits
+    else
+      self.reset_hits
+    end
+
+    if (@consecutive_hits >= @@HITS2LOSE) || self.dead
+      self.reset_hits
+      lose = true
+    end
+    lose
+  end
+
+  def reset_hits
+    @consecutive_hits = 0
+  end
+
+  def got_wounded
+    @health -= 1
+  end
+
+  def inc_consecutive_hits
+    @consecutive_hits += 1
+  end
